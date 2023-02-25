66780653997b8760e7a6108669d16fba407cf97d
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-25 12:55:09 -0300

    feat: initial carrousel implemented

--- src/components/TestimoniesSection/TestimonyCard/index.jsx
+++ src/components/TestimoniesSection/TestimonyCard/index.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import profileImage from '../../../assets/profile.webp';
 import styles from './style.module.css';
 
-export default function TestimonyCard() {
+export default function TestimonyCard({ testimony, image, name }) {
   return (
     <div className={styles.card}>
-      <q className={styles.testimony}>
-        Eu sou cliente do Beautysalon há 5 anos e não troco por nada! Certamente
-        meu cabelo mudou muito depois que comecei a tratar somente com produtos
-        naturais e veganos. São profissionais incríveis e qualificados.
-      </q>
+      <q className={styles.testimony}>{testimony}</q>
       <span className={styles.profile_wrapper}>
         <img
-          src={profileImage}
+          src={image}
           className={styles.image}
-          alt="Imagem de perfil de Wanessa Souza"
+          alt={`Imagem de perfil de ${name}`}
         />
-        <span className={styles.name}>Wanessa Souza</span>
+        <span className={styles.name}>{name}</span>
       </span>
     </div>
   );
--- src/components/TestimoniesSection/index.jsx
+++ src/components/TestimoniesSection/index.jsx
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TestimonyCard from './TestimonyCard';
+import wanessaImage from '../../assets/wanessa-profile.webp';
+import lunaImage from '../../assets/luna-profile.webp';
 import styles from './style.module.css';
 
+const testimonyList = [
+  {
+    image: wanessaImage,
+    name: 'Wanessa Souza',
+    testimony:
+      'Eu sou cliente do Beautysalon há 5 anos e não troco por nada! Certamente meu cabelo mudou muito depois que comecei a tratar somente com produtos naturais e veganos. São profissionais incríveis e qualificados.',
+  },
+  {
+    image: lunaImage,
+    name: 'Luna Falcão',
+    testimony:
+      'Minha mãe frequenta o salão há anos e me levou um dia para conhecer. Minha experiência foi incrível, eu continuo fazendo o a terapia capilar e isso salvou o meu cabelo. Adoro todos os profissionais do Beautysalon.',
+  },
+];
+
+function renderButtons(ammount, onClick) {
+  const buttons = [];
+  for (let counter = 0; counter < ammount; counter += 1) {
+    buttons.push(
+      <button
+        type="button"
+        onClick={() => onClick(counter)}
+        className={styles.button}
+      />
+    );
+  }
+  return buttons;
+}
+
 export default function TestimoniesSection() {
+  const [selected, setSelected] = useState(0);
+
   return (
     <section className={styles.section}>
       <h1 className={styles.section_title}>
         Depoimentos de quem já passou por aqui
       </h1>
-      <TestimonyCard />
+
+      <div className={styles.carrousel}>
+        <ul>
+          {testimonyList.map(({ image, name, testimony }, index) => (
+            <li
+              style={index === selected ? { display: 'flex' } : {}}
+              className={styles.list_item}
+            >
+              <TestimonyCard image={image} name={name} testimony={testimony} />
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.buttons_wrapper}>
+          {renderButtons(2, setSelected)}
+        </div>
+      </div>
     </section>
   );
 }
