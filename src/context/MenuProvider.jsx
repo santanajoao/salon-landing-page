498291037bf2310071331bb26bef1c182a48d8b1
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-03-15 15:34:29 -0300

    perf: add useMemo on provider

--- src/context/MenuProvider.jsx
+++ src/context/MenuProvider.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import MenuContext from './MenuContext';
 
 export default function MenuProvider({ children }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const values = { isMenuOpen, setIsMenuOpen };
+  const values = useMemo(
+    () => ({ isMenuOpen, setIsMenuOpen }),
+    [isMenuOpen, setIsMenuOpen]
+  );
 
   return <MenuContext.Provider value={values}>{children}</MenuContext.Provider>;
 }
